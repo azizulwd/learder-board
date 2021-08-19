43cd9c8f70121b4a4b8080ff10bed99394d0dda3
azizulwd <azizulislamgd@gmail.com>
2021-08-19 23:56:47 +0600

    background color added in players section

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ function subTitleColor(titleId){
     subTitle.style.color = "#ff5722";
 }
 
+// Calling function
 subTitleColor("top_player");
 subTitleColor("top_Blogs");
 subTitleColor("latest_course");
 
+// Change header background
 let headerBg = document.getElementById("header");
 headerBg.style.backgroundColor = "#ff5722";
+
+
+// Change player background color
+let player = document.querySelectorAll(".players div");
+for(let allPlayer of player){
+    allPlayer.style.backgroundColor = "rgba(255, 87, 34, 0.2)";
+    allPlayer.style.padding = "15px 0 15px 15px";
+    allPlayer.style.borderRadius = "5px";
+}
