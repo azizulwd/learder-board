58e56d6b65ef1397ab13ba354a7be88f512e5a88
azizulwd <azizulislamgd@gmail.com>
2021-08-19 22:45:50 +0600

    change primary color

---
+++ index.js
@@ -0,0 +1,12 @@
+// Change subtitle color
+function subTitleColor(titleId){
+    let subTitle = document.getElementById(titleId);
+    subTitle.style.color = "#ff5722";
+}
+
+subTitleColor("top_player");
+subTitleColor("top_Blogs");
+subTitleColor("latest_course");
+
+let headerBg = document.getElementById("header");
+headerBg.style.backgroundColor = "#ff5722";
