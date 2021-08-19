2f7aeb985b22fdfd7693cbce8d6901b85efda065
azizulwd <azizulislamgd@gmail.com>
2021-08-20 01:47:46 +0600

    update functionality

--- index.js
+++ index.js
@@ -21,3 +21,42 @@ for(let allPlayer of player){
     allPlayer.style.padding = "15px 0 15px 15px";
     allPlayer.style.borderRadius = "5px";
 }
+
+
+// Footer (ul list)
+document.getElementById("add_Link").addEventListener("click", function(){
+    let footerLink = document.getElementById("footer_links");
+    let createLi = document.createElement("li");
+    createLi.innerText = "New List";
+
+    footerLink.appendChild(createLi);
+});
+
+
+// Increasing input number
+document.getElementById("increase_number").addEventListener("click", function(){
+    let numberInput = document.getElementById("number_input");
+    let numberInputText = numberInput.value;
+
+    if(numberInputText < 5){
+        numberInputText = parseInt(numberInput.value) + 1;
+    }
+    else if(numberInputText == 5){
+        document.getElementById("increase_number").setAttribute("disabled", true);
+    }
+    numberInput.value = numberInputText;
+});
+
+document.getElementById("number_input").addEventListener("keyup", function(){
+    let numberInput = document.getElementById("number_input");
+    let numberInputText = numberInput.value;
+    numberInputText = parseInt(numberInput.value);
+
+    if(numberInputText == 5){
+        document.getElementById("increase_number").setAttribute("disabled", true);
+    }
+    else{
+        document.getElementById("increase_number").removeAttribute("disabled");
+    }
+    numberInput.value = numberInputText;
+});
